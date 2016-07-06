54ad24d674e45e44ad8f88d74ad69a2cdf583ab5
Xsess <snyder.xander@mykolab.com>
2016-07-06 14:18:49 -0700

    Correct transition lengths

--- Revo2.R
+++ Revo2.R
@@ -29,35 +29,53 @@ dt_revo.trimmed[,data_year:=seq_along(year),by=list(ccname)]
 dt_revo.trimmed[,transition := polity < -10, by = ccname]
 
 
-########
+###Creates last_polity column using na.locf
+###Side effect from split year+1 / merge technique: Strips out initial state year for all countries, 
+####since need before & after data to calculate polity change 
 all_countries <- dt_revo.trimmed
 all_countries[,polity_cens:=ifelse(polity< -10, NA,polity)]
 all_countries[,polity_carryforward:=na.locf(polity_cens,na.rm=F),by=ccname]
 
 
-prev_country_data <- all_countries[,list(ccname, year = year+1, last_leader = leader, last_revo = revolutionaryleader, last_polity = polity_carryforward)]
 
 all_countries = merge(all_countries, prev_country_data, by = c("ccname", "year"))
-View(all_countries[,list(ccname,year,leader,last_leader,revolutionaryleader,last_revo,polity,polity_cens,polity_carryforward,last_polity)])
+all_countries[, ':='(transition_length = 1:.N), by = list(leader, transition)]
+all_countries[, transition_length:=ifelse(transition == F, 0, transition_length)]
+#View(all_countries[,list(ccname,year,leader,last_leader,revolutionaryleader,last_revo,polity,polity_cens,polity_carryforward,last_polity)])
 
-test=all_countries[ccname=="Afghanistan"]
-test[,polity_carryforward:=na.locf(polity_cens),by=ccname]
+#all_countries[,transition_start:=c(0,diff(transition))] ---> IS THIS STILL NECESSARY?
+#View(all_countries[,list(ccname, year, leader, revolutionaryleader, polity, transition, transition_start)])
+
+#test=all_countries[ccname=="Afghanistan"]
+#test[, ':='(transition_length = 1:.N), by = list(leader,transition)]
+#test[,transition_length:=ifelse(transition == FALSE, 0, transition_length)]
+#[ , `:=`( COUNT = .N , IDX = 1:.N ) , by = VAL ]
+#test[, transition_length:=ifelse(transition == TRUE, transition + shift(transition_length, 1L, type = "lag"),0) ]
+#http://stackoverflow.com/questions/19869145/counting-in-r-data-table
+
+#prev_country_data <- all_countries[,list(ccname, year = year+1, last_leader = leader, last_revo = revolutionaryleader, last_polity = polity_carryforward)]
+#test[,polity_carryforward:=na.locf(polity_cens),by=ccname]
 #######
 
 
 #extract years that are both transition and revo leader
 dt_transrevo=dt_revo.trimmed[transition==1&revolutionaryleader==1]
 test=dt_revo.trimmed[ccname=="Afghanistan"]
+
+#Might work for determining length of regime
 leaders=dt_revo.trimmed[!duplicated(dt_revo.trimmed[,list(leader,ccname)])]
 leaders[,startyear:=as.numeric(strftime(strptime(startdate,"%d/%m/%Y"),"%Y"))]
 leaders[,endyear:=as.numeric(strftime(strptime(enddate,"%d/%m/%Y"),"%Y"))]
-leaders[,regime_length:=endyear-startyear+1]
-pre_revo=leaders[,list(ccname,year=start_year-1)]
+leaders[,regime_length:=endyear-startyear+1]  ###Why is this startyear+1 instead of just startyear?
+
+pre_revo=leaders[,list(ccname,year=startyear-1)]  #Was this to be merged with leaders?
 
 leaders[,regime_type:="non-revolutionary"]
 leaders[revolutionaryleader==1&transition==1,regime_type:="transition revolutionary"]
 leaders[revolutionaryleader==1&transition==0,regime_type:="non-transition revolutionary"]
 
+View(leaders[,list(ccname, leader, year, startdate, enddate, startyear, endyear, revolutionaryleader, 
+                   polity, regime_length, regime_type)])
 head(dt_transrevo)
 
 dt_revo.trimmed[,trans_diff:=c(0,diff(transition)),by=ccname]
