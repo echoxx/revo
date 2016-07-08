9a212858f3c068fdbe43189c7195b6363e895edb
Xsess <snyder.xander@mykolab.com>
2016-07-08 16:14:09 -0700

    transition length for adjacent transitions working correctly throughout

--- Revo2.R
+++ Revo2.R
@@ -56,53 +56,77 @@ all_countries[,polity_cens:=ifelse(polity< -10, NA,polity)]
 all_countries[,polity_carryforward:=na.locf(polity_cens,na.rm=F),by=ccname]
 
 #Only picks up transitions by leaders, not adjacent transitions by multiple leaders
+
+
 ###ATTEMPT TO PICK UP ADJACENT LEADERS/TRANSITIONS
 all_countries[,transition_start_end:=c(0,diff(transition)), by = list(ccname)] #---> IS THIS STILL NECESSARY?
 all_countries[,transition_start_count:=1L:.N, by = list(ccname, transition, transition_start_end)]
-all_countries[, transition_length:=(1L:.N), by = list()]
+all_countries[,transition_start_count:=ifelse(transition_start_end != 1, 0, transition_start_count)]
 
-###TEST
-test <- all_countries2[c( "Afghanistan", "Cambodia")]
-
-test[,transition_start_count:=1L:.N, by = list(ccname, transition, transition_start_end)] #This works
-test[,transition_start_count:=ifelse(transition_start_end != 1, 0, transition_start_count)] #Tallys # of new transition starts
-
-tempcounter <- numeric(length(test$ccname))
-for(i in seq_along(test$ccname) ) {
-  if(test$transition_start_count[i] > 0 && test$transition[i] == T && test$transition_start_end[i] ==1 
-     && test$ccname[i] == test$ccname[i-1]) {
-    tempcounter[i] <- test$transition_start_count[i]
-  } else if (test$transition_start_count[i] == 0 && test$transition[i] == T && test$transition_start_end[i] == 0 
-             && test$ccname[i] == test$ccname[i-1]) {
+tempcounter <- numeric(length(all_countries$ccname))
+for(i in seq_along(all_countries$ccname) ) {
+  if (is.na(all_countries$transition[i]) | is.na(all_countries$transition_start_count[i])) { 
+    tempcounter[i] <- 0
+    } else if (all_countries$transition_start_count[i] > 0 && all_countries$transition[i] == T && 
+     all_countries$transition_start_end[i] ==1 && all_countries$ccname[i] == all_countries$ccname[i-1]) {
+    tempcounter[i] <- all_countries$transition_start_count[i]
+    } else if (all_countries$transition_start_count[i] == 0 && all_countries$transition[i] == T && 
+             all_countries$transition_start_end[i] == 0 && all_countries$ccname[i] == all_countries$ccname[i-1]) {
     tempcounter[i] <- tempcounter[i-1]
-  } else {
+    } else {
     tempcounter[i] <- 0
   }
 }
+all_countries[,transition_start_count:=tempcounter]
 
-test[,transition_start_count:=tempcounter]
-
-test[, transition_length:=(1L:.N), by = list(ccname, transition_start_count, transition)] #NEED THIS LINE AND ABOVE INE?
-test[transition == F, transition_length:= 0] #2nd command
-
+all_countries[, transition_length:=(1L:.N), by = list(ccname, transition_start_count, transition)] #NEED THIS LINE AND ABOVE INE?
+all_countries[transition == F, transition_length:= 0] #2nd command
 
 
-test_index <- which ( test[,transition_start_end == 1] )
-test[test_index[1]:test_index[2]-1,transition_start_count:=transition_start_count[test_index[1]]]
-test[test_index[1]:test_index[2], year]
+#all_countries[,transition_start_end:=c(0,diff(transition)), by = list(ccname)] #---> IS THIS STILL NECESSARY?
 
-test[,transition_start_count[test_index[1]:(test_index[2]-1)]] #subsets correctly
-test$transition_start_count[test_index[1]:(test_index[2]-1)] <- test$transition_start_count[test_index[1]] # how to generalize?
+###TEST
+#test <- all_countries2[c( "Afghanistan", "Cambodia")]
+
+#test[,transition_start_count:=1L:.N, by = list(ccname, transition, transition_start_end)] #This works
+#test[,transition_start_count:=ifelse(transition_start_end != 1, 0, transition_start_count)] #Tallys # of new transition starts
+
+#tempcounter <- numeric(length(test$ccname))
+#for(i in seq_along(test$ccname) ) {
+#  if(test$transition_start_count[i] > 0 && test$transition[i] == T && test$transition_start_end[i] ==1 
+#     && test$ccname[i] == test$ccname[i-1]) {
+#    tempcounter[i] <- test$transition_start_count[i]
+#  } else if (test$transition_start_count[i] == 0 && test$transition[i] == T && test$transition_start_end[i] == 0 
+#             && test$ccname[i] == test$ccname[i-1]) {
+#    tempcounter[i] <- tempcounter[i-1]
+#  } else {
+#    tempcounter[i] <- 0
+#  }
+#}
+
+#test[,transition_start_count:=tempcounter]
+
+#test[, transition_length:=(1L:.N), by = list(ccname, transition_start_count, transition)] #NEED THIS LINE AND ABOVE INE?
+#test[transition == F, transition_length:= 0] #2nd command
+
+
+
+#test_index <- which ( test[,transition_start_end == 1] )
+#test[test_index[1]:test_index[2]-1,transition_start_count:=transition_start_count[test_index[1]]]
+#test[test_index[1]:test_index[2], year]
+
+#test[,transition_start_count[test_index[1]:(test_index[2]-1)]] #subsets correctly
+#test$transition_start_count[test_index[1]:(test_index[2]-1)] <- test$transition_start_count[test_index[1]] # how to generalize?
 
 #test[, transition_start_count:=ifelse(transition_start_end == 1, (1L:.N), 0), by = list(transition_start_end)] 
 #test[, transition_length:=(1L:.N), by = list(leader, transition)] 
 ###END TEST
 
 ###OLD CODE
-all_countries[, ':='(transition_length = 1L:.N), by = list(leader, transition)] 
+#all_countries[, ':='(transition_length = 1L:.N), by = list(leader, transition)] 
 
-all_countries[, transition_length:=ifelse(transition == F, 0, transition_length)]
-all_countries[,transition_start_end:=c(0,diff(transition)), by = list(ccname)] #---> IS THIS STILL NECESSARY?
+#all_countries[, transition_length:=ifelse(transition == F, 0, transition_length)]
+#all_countries[,transition_start_end:=c(0,diff(transition)), by = list(ccname)] #---> IS THIS STILL NECESSARY?
 ###END OLD CODE
 
 
